7a7eb3cdd4620d005136119381e16ae8bfb8b089
PetrovIliya <52219078+PetrovIliya@users.noreply.github.com>
2019-06-27 12:15:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
   include('vendor/autoload.php'); //Подключаем библиотеку
   use Telegram\Bot\Api; 
-  echo"hello";
+  $telegram = new Api('831061547:AAFwm0s2dLQIWLhRHJljKVVRv4aTzwpbgI0');
+  $result = $telegram -> getWebhookUpdates();
+  echo $telegram;
 ?>
