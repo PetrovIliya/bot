e354dbc0b74d4ea794c70606b04cd819d216e65b
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-06-30 00:39:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-  include('vendor/autoload.php'); 
+  const TOKEN = '831061547:AAFwm0s2dLQIWLhRHJljKVVRv4aTzwpbgI0';
+  $url = 'https://api.telegram.org' . TOKEN . '/getUpdates';
+  echo $url;
+
+  /*include('vendor/autoload.php'); 
   use Telegram\Bot\Api; 
   $telegram = new Api('831061547:AAFwm0s2dLQIWLhRHJljKVVRv4aTzwpbgI0'); 
   $result = $telegram -> getWebhookUpdates(); 
@@ -16,7 +20,7 @@
     } else {
      	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Запрос не является командой" ]);
     }
-  }
+  } */
 
 
 ?>
