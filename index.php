e8a614c33174d602e984612f40b9e345fe6900d2
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-01 01:58:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
     case 'видео':
       sendRequest('sendMessage', ['chat_id' => $chat_id,
                                   'text' => 'видео']);
-      if($requestWords[1] && $requestWords[2]) {;
+      if($requestWords[1] && $requestWords[2]) {
         if(is_numeric($requestWords[2]) && $requestWords[2] <= MAX_VIDEOS){
            //$dataBySearch = $video->search($requestWords[1], $requestWords[2]); 
         } elseif(!is_numeric($requestWords[2]) {
