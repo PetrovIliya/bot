136276b1f4e3bec3acee33b644e20eb970a46338
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-06-30 22:39:59 +0300

    Update info.php

--- src/info.php
+++ src/info.php
@@ -2,6 +2,7 @@
   const YT_KEY = 'AIzaSyBW_jucSlgbrmgdDCV1m7Voy7aE6R1bil8';
   const TOKEN = '831061547:AAFwm0s2dLQIWLhRHJljKVVRv4aTzwpbgI0';
   const BASE_URL = 'https://api.telegram.org/bot' . TOKEN . '/';
+  const YT_URL = 'https://www.youtube.com/watch?v=';
   use Telegram\Bot\Api;
 
   $telegram = new Api('831061547:AAFwm0s2dLQIWLhRHJljKVVRv4aTzwpbgI0');
