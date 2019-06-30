73cf6d7d19cde992d43a298aec4a9ca38ee1a507
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-06-30 17:34:28 +0300

    Add files via upload

---
+++ src/info.php
@@ -0,0 +1,18 @@
+<?php 
+  const YT_KEY = 'AIzaSyBW_jucSlgbrmgdDCV1m7Voy7aE6R1bil8';
+  const TOKEN = '831061547:AAFwm0s2dLQIWLhRHJljKVVRv4aTzwpbgI0';
+  const BASE_URL = 'https://api.telegram.org/bot' . TOKEN . '/';
+  $telegram = new Api('831061547:AAFwm0s2dLQIWLhRHJljKVVRv4aTzwpbgI0');
+  $update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
+  $chat_id = $update['message']['chat']['id'];
+  $request = $update['message']['text'];
+  $user_first_name = $update['message']['from']['first_name'];
+  $user_last_name = $update['message']['from']['last_name'];
+  $keyboard = [["/музыка"],["/видео"]];
+  $comands = [
+               '/start - начало работы',
+               '/help - список команд',
+               '/видео название видео - поиск видео',
+               '/музыка название песни - поиск музыки'
+             ];
+?>
