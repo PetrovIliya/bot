83b305fb09d69de59a2c7583e61c7d94c997bf52
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-06-30 23:22:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,38 +2,7 @@
   require_once('vendor/autoload.php'); 
   require_once('src/info.php');
   require_once('src/YT_func.php');
-
-  function sendRequest($method, $params = []) {
-    if(!empty($params)) {
-      $url = BASE_URL . $method . '?' . http_build_query($params);
-    } else {
-      $url = BASE_URL . $method;
-    }
-    return  json_decode(file_get_contents($url), JSON_OBJECT_AS_ARRAY);
-  }
-
-
-
-  if ($request == '/start') {
-   $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard,
-                                                   'resize_keyboard' => true,
-                                                   'one_time_keyboard' => false 
-                                                  ]); 
-    
-   sendRequest('sendMessage', ['chat_id' => $chat_id, 
-                               'text' => 'Добро пожаловать ' . $user_first_name . ' ' . $user_last_name . '!',
-                               'reply_markup' => $reply_markup 
-                              ]);
-    
-    
-  } elseif ($request == '/help') {
-      foreach($comands as $comand) {
-      sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $comand . ' ']);
-    }
-  } else {
-    sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => 'Запрос не является командой, со списком доступных команд можно ознакомится с помощью /help']);
-  }
-
+  require_once('src/TG_func.php')
  
 
  $dataBySearch = $video->search('космос', 2); 
