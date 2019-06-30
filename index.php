cb6edb57a8639304c0cdf0e2b97e03ba25d61eb0
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-06-30 17:56:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
   include('vendor/autoload.php'); 
   include('src/YT_func.php');
   include('src/info.php');
-  $telegram = new Api('831061547:AAFwm0s2dLQIWLhRHJljKVVRv4aTzwpbgI0');
+  
   use Telegram\Bot\Api; 
+  $telegram = new Api('831061547:AAFwm0s2dLQIWLhRHJljKVVRv4aTzwpbgI0');
 
   function sendRequest($method, $params = []) {
     if(!empty($params)) {
