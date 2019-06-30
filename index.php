f5930e633f557097bd1c84b40867123ef3ec6c66
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-06-30 20:13:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
   $dataBySearch = $video->search('космос', 1); 
   json_decode($dataBySearch, JSON_OBJECT_AS_ARRAY); 
   //$dataBySearch = $video->getDataVideo($dataBySearch->getItems());
- $videoId = $dataBySearch['items']['modelData'];
+ $videoId = $dataBySearch['items']['id'];
  var_dump($videoId); 
 
 /*
