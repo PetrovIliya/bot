ef5dec068625fcb914c1dbe8d3c1a00cff6a1d4c
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-06-30 21:12:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
  // $dataBySearch = $video->search('космос', 1); 
  // $dataBySearch = $video->getDataVideo($dataBySearch->getItems());
 $some = 'https://www.googleapis.com/youtube/v3/search?part=snippet%20&q=serj&key=AIzaSyBW_jucSlgbrmgdDCV1m7Voy7aE6R1bil8';
-json_decode(file_get_contents($some), JSON_OBJECT_AS_ARRAY);
+json_decode(file_get_contents($some));
 var_dump($some);
 
 /*
