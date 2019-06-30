ba669a798973b3430ca9d0314c0ef27b40f4b391
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-06-30 21:22:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -38,8 +38,8 @@
  // $dataBySearch = $video->getDataVideo($dataBySearch->getItems());
 $some = 'https://www.googleapis.com/youtube/v3/search?part=snippet%20&q=serj&key=AIzaSyBW_jucSlgbrmgdDCV1m7Voy7aE6R1bil8';
 $dec = json_decode(file_get_contents($some), JSON_OBJECT_AS_ARRAY);
-$video_id = $dec['id']['videoId'];
-echo $video_id;
+$video_id = $dec['items']['id'];
+var_dump($video_id);
 /*
 ?>
   <pre>
