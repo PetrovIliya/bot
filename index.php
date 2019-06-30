5d801e1c387dd4db3607c8801efbceb9274a2b9d
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-01 01:37:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -24,6 +24,21 @@
     case 'видео':
       sendRequest('sendMessage', ['chat_id' => $chat_id,
                                   'text' => 'видео']);
+      if($requestWords[1] && $requestWords[2]) {;
+        if(is_numeric($requestWords[2]) && $requestWords[2] <= MAX_VIDEOS){
+           //$dataBySearch = $video->search($requestWords[1], $requestWords[2]); 
+        } elseif(!is_numeric($requestWords[2]) {
+           sendRequest('sendMessage', ['chat_id' => $chat_id,
+                                       'text' => '''количество'' - должно быть целым числом']);
+        } else {
+          sendRequest('sendMessage', ['chat_id' => $chat_id,
+                                       'text' => '''количество'' - не может превышать'. MAX_VIDEOS]);
+        }
+        
+      } else {
+        sendRequest('sendMessage', ['chat_id' => $chat_id,
+                                       'text' => 'не верно указаны параметры']);
+      }
     default: 
       sendRequest('sendMessage', ['chat_id' => $chat_id,
                                   'text' => 'Запрос не является командой, со списком доступных команд можно ознакомится с помощью /help']);
