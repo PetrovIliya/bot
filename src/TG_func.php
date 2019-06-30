566996704ef61312a7278ecab4ace4203c787e61
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-06-30 23:23:33 +0300

    Add files via upload

---
+++ src/TG_func.php
@@ -0,0 +1,33 @@
+<?php
+
+  function sendRequest($method, $params = []) {
+    if(!empty($params)) {
+      $url = BASE_URL . $method . '?' . http_build_query($params);
+    } else {
+      $url = BASE_URL . $method;
+    }
+    return  json_decode(file_get_contents($url), JSON_OBJECT_AS_ARRAY);
+  }
+
+  function checkRequest($request, $param) {
+    if ($request == '/start') {
+   $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard,
+                                                   'resize_keyboard' => true,
+                                                   'one_time_keyboard' => false 
+                                                  ]); 
+    
+   sendRequest('sendMessage', ['chat_id' => $chat_id, 
+                               'text' => 'Добро пожаловать ' . $user_first_name . ' ' . $user_last_name . '!',
+                               'reply_markup' => $reply_markup 
+                              ]);
+    
+    
+  } elseif ($request == '/help') {
+      foreach($comands as $comand) {
+      sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $comand . ' ']);
+    }
+  } else {
+    sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => 'Запрос не является командой, со списком доступных команд можно ознакомится с помощью /help']);
+  }
+  }  
+?>
