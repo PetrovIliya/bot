5002d1df8cc8c191c1d1f9c2f21d6c184fbb43b7
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-06-30 23:51:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,26 +4,26 @@
   require_once('src/YT_func.php');
   require_once('src/TG_func.php');
   
-  if ($request == '/start') {
-   $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard,
-                                                   'resize_keyboard' => true,
-                                                   'one_time_keyboard' => false 
-                                                  ]); 
+  switch ($request): 
+    case '/start': 
+      $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard,
+                                                     'resize_keyboard' => true,
+                                                     'one_time_keyboard' => false]); 
     
-   sendRequest('sendMessage', ['chat_id' => $chat_id, 
-                               'text' => 'Добро пожаловать ' . $user_first_name . ' ' . $user_last_name . '!',
-                               'reply_markup' => $reply_markup 
-                              ]);
-    
-    
-  } elseif ($request == '/help') {
+     sendRequest('sendMessage', ['chat_id' => $chat_id, 
+                                 'text' => 'Добро пожаловать ' . $user_first_name . ' ' . $user_last_name . '!',
+                                 'reply_markup' => $reply_markup]); 
+      break;
+    case  'команды':
       foreach($comands as $comand) {
-      sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $comand . ' ']);
-    }
-  } else {
-    sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => 'Запрос не является командой, со списком доступных команд можно ознакомится с помощью /help']);
-  }
-
+        sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $comand . ' ']);
+      }
+      break;
+    default: 
+      sendRequest('sendMessage', ['chat_id' => $chat_id,
+                                  'text' => 'Запрос не является командой, со списком доступных команд можно ознакомится с помощью /help']);
+  endswitch
+  
  $dataBySearch = $video->search('космос', 2); 
   // $dataBySearch = $video->getDataVideo($dataBySearch->getItems());
  $video_ids = $dataBySearch -> items[0] -> id['videoId'];
