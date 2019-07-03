750ee73a48cc76d64ac6a3bbcb3b29c9f6f859df
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-03 18:13:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -60,12 +60,10 @@
         sendRequest('sendMessage', ['chat_id' => $chatId, 'text' => 'не верно указаны параметры']);
       }
       break;
-/*
     case 'история':
     case 'История':
      $isFound = true;
      $isFound = sendUserHistory($userData, $chatId);
-     */ 
     default: 
       sendRequest('sendMessage', ['chat_id' => $chatId,
                                  'text' => 'Запрос не является командой, со списком доступных команд можно ознакомится с помощью запроса "команды"']);
