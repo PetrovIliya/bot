e87e7f270f1615e9d6b68cb7890df6ed07a7adee
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-03 16:10:00 +0300

    Add files via upload

---
+++ src/dataBase.php
@@ -0,0 +1,54 @@
+<?php
+const QUERY_LENGTH = 5;
+
+  function placeForInsert($userData) {
+   $isEmpty = false;
+   for($i = 1; $i <= QUERY_LENGTH; $i++) {
+     if(empty($userData['userQuery' . $i])) {
+       $isEmpty = true;
+       break;
+   }
+     }
+     if ($isEmpty) {
+       return $i;
+     } else {
+       return ++$i;
+     }
+  }
+
+  function insertUserHistory ($db, $userData, $serchResult, $userId) {
+    if($userData) {
+      $insertPlace = placeForInsert($userData);
+      if($insertPlace <= QUERY_LENGTH) {
+        $userQuery = 'userQuery' . $insertPlace;
+        $data = [$userQuery => $serchResult];
+        $db->where('userId', $userId);
+        $db->update('userHistory', $data);
+      } else {
+        $data=['userQuery1' => $userData['userQuery2'],
+               'userQuery2' => $userData['userQuery3'],
+               'userQuery3' => $userData['userQuery4'],
+               'userQuery4' => $userData['userQuery5'],
+               'userQuery5' => $serchResult      ];
+        $db->where('userId', $userId);
+        $db->update('userHistory', $data);
+      }
+    } else {
+      $data=['userId' => $userId,
+          'userQuery1' => $serchResult];
+      $db->insert('userHistory', $data);
+    }
+  }
+
+
+  function sendUserHistory($userData, $chatId) {
+     if($userData) {
+     foreach($userData as $fieldName => $fieldValue) {
+       if($fieldName !== 'userId' && !empty($fieldValue)) {
+         sendRequest('sendMessage', ['chat_id' => $chatId, 'text' => $fieldValue]);
+       } elseif(empty($fieldValue)) {
+         break;
+       }
+     }
+    }
+  }
