a7ecaf828631e2576b5cad3aa9493d570efa4178
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-03 22:30:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
   $telegram = new Api('831061547:AAFwm0s2dLQIWLhRHJljKVVRv4aTzwpbgI0');
   $video = new YouTubeVideo();
-  dataBaseInit($db);
+  $db = dataBaseInit();
   $update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
   $chatId = $update['message']['chat']['id'];
   $request = $update['message']['text'];
