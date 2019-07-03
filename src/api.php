04fa733bb3026bda301e6169312a16b1d703aa9a
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-04 01:53:27 +0300

    Update api.php

--- src/api.php
+++ src/api.php
@@ -16,7 +16,7 @@
     } else {
       $url = TELEGRAM_URL . $method;
     }
-    return  json_decode(file_get_contents($url)); 
+    return  json_decode(file_get_contents($url), JSON_OBJECT_AS_ARRAY); 
   }
   function sendVideos($data, $quantity, $chatId) {
     for($i=0; $i < $quantity; $i++) {
