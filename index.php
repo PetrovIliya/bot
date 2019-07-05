96eaf74b1f5bff40b94673f9f7f88489bffea8c5
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-05 18:02:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
   require_once('src/telegramm.php');
   require_once('src/youtube.php');
   require_once ('src/dataBase.php');
+  require_once ('src/botLogic.php');
  
   startBot($update, $db, $video);
