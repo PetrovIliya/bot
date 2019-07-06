efd7b8f7c195597f314a398ddca3d5cb2fa8fb70
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-06 19:55:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,16 @@
   require_once('vendor/autoload.php'); 
  // require_once('src/telegrammAPI.php');
   require_once('src/youtubeAPI.php');
-//  require_once ('src/dataBase.php');
+  require_once ('src/dataBase.php');
   //require_once ('src/botLogic.php');
 
   $video = youTubeInit();
-//  $db = dataBaseInit();
+  $db = dataBaseInit();
  // $update = telegramInit();
   $some = $video -> getCategory();
-  var_dump($some);
+  foreach($some as $som)
+  {
+    $data = [ 'categoriesName' => $som];
+    $db->insert('categories', $data);
+  };
   //startBot($update, $db, $video);
