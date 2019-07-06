6af3b57ce0a282f76853832cacc46926c2ddf581
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-07 02:59:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,6 @@
   //$db = dataBaseInit();
   //$update = telegramInit();
   
-  $test = $video -> getPopularVideosByCategory('Развлечения', 10, 'RU');
+  $test = $video -> getPopularVideosByCategory('Шоу', 10, 'RU');
   var_dump($test);
  // startBot($update, $db, $video);
