790250fd20fb404fadffbd3197922058bb621c43
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-07 02:15:41 +0300

    Update botLogic.php

--- src/botLogic.php
+++ src/botLogic.php
@@ -1,5 +1,10 @@
 <?php
   require_once('notification.php');
+  const PARAMETRS_ERROR_MESSAGE = 'не верно указаны параметры';
+  const QUANTINTY_ERROR_MESSAGE = '<количество> - должно быть целым числом';
+  const MAX_QUANTINTY_ERROR_MESSAGE = '<количество> - не может превышать 10';
+  const UNSCRIBE_MESSAGE = 'Вы успешно отписаны от уведомлений';
+  const USER_ERROR_MESSAGE = 'Запрос не является командой, со списком доступных команд можно ознакомится с помощью запроса <команды>"';
   const DEFAULT_HISTORY_QUANTINTY = 5;
   const DEFALT_CATEGORY = 'развлечения';
   const MAX_VIDEOS = 10;
@@ -8,6 +13,7 @@
   const ALL_COMMANDS_COMMAND = 'команды';
   const HISTORY_COMMAND = 'история';
   const CATEGORIES_COMAND = 'категории';
+  const UNSCRIBE_COMMAND = 'отписаться';
   const EXCEPTIONS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789./?=-_:';  
   const COMMANDS = ['знаки "<" и ">" служат только для обозначения разделов команд, набирать их не стоит', 
                    'команды - список команд',
@@ -38,11 +44,11 @@
       } 
       elseif(!is_numeric($lastWord))
       {
-          sendMessage('"количество" - должно быть целым числом', $chatId);
+          sendMessage(QUANTINTY_ERROR_MESSAGE, $chatId);
       } 
       else
       {
-          sendMessage('"количество" - не может превышать ' . MAX_VIDEOS, $chatId);
+          sendMessage(MAX_QUANTINTY_ERROR_MESSAGE, $chatId);
       }     
   }  
 
@@ -56,7 +62,7 @@
       } 
       else 
       {
-          sendMessage('не верно указаны параметры', $chatId);
+          sendMessage(PARAMETRS_ERROR_MESSAGE, $chatId);
       }
   }
 
@@ -75,18 +81,18 @@
           }
           else
           {
-              sendMessage('"количество" - должно быть целым числом', $chatId);
+              sendMessage(QUANTINTY_ERROR_MESSAGE, $chatId);
           }  
       }
   }
 
   function categoriesLogicHandler($db, $chatId)
   {
-   $categories = getColumn($db, 'categoriesName', 'categories');
-   foreach($categories as $category)
-   {
-     sendMessage($category, $chatId);
-   }  
+       $categories = getColumn($db, 'categoriesName', 'categories');
+       foreach($categories as $category)
+       {
+           sendMessage($category, $chatId);
+       }  
   }  
 
   function botLogicHandler($db, $video, $request, $greatings, $chatId, $userId)
@@ -113,10 +119,10 @@
               break;
           case UNSCRIBE_COMMAND
               unscribe($db, $chatId);
+              sendMessage(UNSCRIBE_MESSAGE, $chatId);
               break;
           default: 
-             sendMessage('Запрос не является командой, со списком доступных команд можно ознакомится с помощью запроса "команды"',
-                          $chatId);
+             sendMessage(USER_ERROR_MESSAGE, $chatId);
           endswitch;
   }  
 
