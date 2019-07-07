9f83513343e6e20ad404c3abc9ef9f2fc8df0c8d
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-07 04:30:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,18 +6,6 @@
   require_once ('src/botLogic.php');
 
   $video = youTubeInit();
-  //$db = dataBaseInit();
-  //$update = telegramInit();
-
-  
-  $test = $video -> getPopularVideosByCategory('1', 1);
-  $item = $test["items"][0]["id"];
-  var_dump($item);
-  ?>
-  <pre>
-  <?php  
-  var_dump($test); 
-  ?>
-  <pre> 
-  <?php  
- // startBot($update, $db, $video);
+  $db = dataBaseInit();
+  $update = telegramInit();
+  startBot($update, $db, $video);
