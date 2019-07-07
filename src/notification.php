379d0f78cd80390c0d50d63e3f0bd5710eb9a440
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-07 12:22:32 +0300

    Update notification.php

--- src/notification.php
+++ src/notification.php
@@ -1,6 +1,6 @@
 <?php 
   require_once('config.php');
-  require_once('autoload.php'); 
+  require_once(dirname(__FILE__).'/../vendor/autoload.php'); 
   require_once('telegrammAPI.php');
   require_once('youtubeAPI.php');
   require_once ('dataBase.php');
