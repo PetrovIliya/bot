97c330a04e037719866048732ab181096ac1eaa4
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-07 12:54:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   require_once('vendor/autoload.php'); 
-//  require_once('src/telegrammAPI.php');
+  require_once('src/telegrammAPI.php');
   require_once('src/youtubeAPI.php');
   require_once ('src/dataBase.php');
  // require_once ('src/botLogic.php');
