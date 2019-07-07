0e937f0eddfcf80e5f904d0e6704c70e74b2dcbd
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-07 10:12:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,8 @@
   require_once('src/youtubeAPI.php');
   require_once ('src/dataBase.php');
   require_once ('src/botLogic.php');
-  require_once ('src/notification.php');
 
   $video = youTubeInit();
   $db = dataBaseInit();
   $update = telegramInit();
   startBot($update, $db, $video);
-
-
-  startNotificationHandler($db, $video);
