be332e4c217df96d0b1a71098f1b229ef32f11dd
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-07 12:51:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
   require_once('vendor/autoload.php'); 
-  require_once('src/telegrammAPI.php');
+//  require_once('src/telegrammAPI.php');
   require_once('src/youtubeAPI.php');
   require_once ('src/dataBase.php');
-  require_once ('src/botLogic.php');
+ // require_once ('src/botLogic.php');
   require_once ('src/notification.php');
 
   $video = youTubeInit();
