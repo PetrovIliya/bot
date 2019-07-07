9b27bd448cf67022396f324c682c4e1f451e5595
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-07 09:05:18 +0300

    Update notification.php

--- src/notification.php
+++ src/notification.php
@@ -29,7 +29,8 @@
           {
               foreach($value as $val)
               {
-                  echo $key;
+                  echo $key . ' ' . $val;
+                  
               }  
           }  
       }  
