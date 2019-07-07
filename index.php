1d80c18037c2dfa1e45e31dba9009ff08bcd2fd4
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-07 03:24:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,6 @@
 
   $some = $video -> getCategory();
   
-  //$test = $video -> getPopularVideosByCategory($some[0]);
-  var_dump($some);
+  $test = $video -> getPopularVideosByCategory($some[1]);
+  var_dump($test);
  // startBot($update, $db, $video);
