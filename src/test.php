177e65c030033a19946e99040c41d9bbb432cac2
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2019-07-10 03:30:21 +0300

    Update test.php

--- src/test.php
+++ src/test.php
@@ -1,5 +1,5 @@
 <?php
 require_once('config.php');
-require_once(dirname(__FILE__).'/../index.php');
+//require_once(dirname(__FILE__).'/../index.php');
 
-sendMessage('Вот так это делается', 819183182);
+    sendMessage('Вот так это делается', 819183182);
